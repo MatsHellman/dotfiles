16b202b962a89ef895499ba22d6ca71cae1bee2a
Mats Hellman <mats.hellman@nixadmins.net>
2026-05-15 01:07:47 +0300

    Tmux configuration changed to follow dracula theme

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = 'v0.2.0',
+  tag = '*',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
     { '<leader>ff', function() require('telescope.builtin').find_files() end, desc = 'Find files' },
